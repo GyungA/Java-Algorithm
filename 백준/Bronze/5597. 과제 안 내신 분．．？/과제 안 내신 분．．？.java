a934ae4d7a6599b96aeeb0183d8ee20655c7872b
고경아 <gga612@naver.com>
2024-06-26 06:19:32 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,30 +1,29 @@
 import java.io.*;
 
 /*
- * 스트링빌더에 1부터 30까지 담는다
- * 일치하는 스트링을 제거한다
- * 남은 스트링을 출력한다
+ * 학생 수만큼 boolean 배열을 만든다
+ * 제출되지 않은(false) 학생의 번호를 출력한다
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] submitted = new boolean[31];
+
+        for (int i = 1; i <= 28; i++) {
+            int submittedStudentNum = Integer.parseInt(br.readLine().trim());
+            submitted[submittedStudentNum] = true;
+        }
+
+        br.close();
+
         StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= 30; i++) {
-            sb.append(i).append("\n");
-        }
-
-        for (int i = 1; i <= 28; i++) {
-            String submittedStudentNum = br.readLine().trim();
-            int studentIdx = sb.indexOf(submittedStudentNum);
-            if (Integer.parseInt(submittedStudentNum) >= 10) {
-                sb.delete(studentIdx, studentIdx+3);
-            } else {
-                sb.delete(studentIdx, studentIdx+2);
+            if (!submitted[i]) {
+                sb.append(i).append("\n");
             }
         }
 
-        sb.setLength(sb.length()-1);
         System.out.println(sb);
     }
 }
