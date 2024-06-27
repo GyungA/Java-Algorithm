adb2b50c4694a2cee26c92abf11c8c4462c87c68
고경아 <gga612@naver.com>
2024-06-27 11:18:49 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -15,14 +15,11 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         char[][] board = new char[M][N];
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             String currentLine = br.readLine();
             for (int j = 0; j < N; j++) {
                 board[i][j] = currentLine.charAt(j);
-                sb.append(board[i][j]);
             }
-            sb.append("\n");
         }
 
         // 왼쪽 위부터 시작점을 옮겨가며, 8*8 사이즈로 반복
@@ -33,59 +30,20 @@ public class Main {
             }
         }
         System.out.println(result);
+        br.close();
     }
 
     public static int calcEachMin(int m, int n, char[][] board) {
-        // 흰색으로 시작
-        int wCount = 0;
+        int wCount = 0; // 흰색으로 시작할 때 다시 칠해야 하는 개수
+        int bCount = 0; // 검은 색으로 시작할 때 다시 칠해야 하는 개수
         for (int i = m; i < m+8; i++) {
             for (int j = n; j < n+8; j++) {
-                if ((i % 2 != 0) && (j % 2 != 0)) {
-                    if (board[i][j] != 'W') {
-                        wCount++;
-                    }
-                }
-                if ((i % 2 != 0) && (j % 2 == 0)) {
-                    if (board[i][j] != 'B') {
-                        wCount++;
-                    }
-                }
-                if ((i % 2 == 0) && (j % 2 != 0)) {
-                    if (board[i][j] != 'B') {
-                        wCount++;
-                    }
-                }
-                if((i % 2 == 0) && (j % 2 == 0)) {
-                    if (board[i][j] != 'W') {
-                        wCount++;
-                    }
-                }
-            }
-        }
-
-        // 검은색으로 시작
-        int bCount = 0;
-        for (int i = m; i < m+8; i++) {
-            for (int j = n; j < n+8; j++) {
-                if ((i % 2 != 0) && (j % 2 != 0)) {
-                    if (board[i][j] != 'B') {
-                        bCount++;
-                    }
-                }
-                if ((i % 2 != 0) && (j % 2 == 0)) {
-                    if (board[i][j] != 'W') {
-                        bCount++;
-                    }
-                }
-                if ((i % 2 == 0) && (j % 2 != 0)) {
-                    if (board[i][j] != 'W') {
-                        bCount++;
-                    }
-                }
-                if((i % 2 == 0) && (j % 2 == 0)) {
-                    if (board[i][j] != 'B') {
-                        bCount++;
-                    }
+                if ((i + j) % 2 == 0) { // 짝수 인덱스
+                    if (board[i][j] != 'W') wCount++;
+                    if (board[i][j] != 'B') bCount++;
+                } else { // 홀수 인덱스
+                    if (board[i][j] != 'B') wCount++;
+                    if (board[i][j] != 'W') bCount++;
                 }
             }
         }
