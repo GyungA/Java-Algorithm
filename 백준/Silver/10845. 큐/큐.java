9148de3303db23d1126e00123fe9880614e5b624
고경아 <gga612@naver.com>
2024-06-27 11:44:18 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 19168 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
+        LinkedList<Integer> queue = new LinkedList<>();
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < count; i++) {
@@ -25,11 +24,12 @@ public class Main {
                 case "empty":
                     sb.append(queue.isEmpty() ? "1" : "0").append("\n");
                     break;
-                case "back":
-                    sb.append(queue.isEmpty() ? "-1" : queue.toArray()[queue.size()-1]).append("\n");
-                    break;
                 case "front":
                     sb.append(queue.isEmpty() ? "-1" : queue.peek()).append("\n");
+                    break;
+                case "back":
+                    sb.append(queue.isEmpty() ? "-1" : queue.getLast()).append("\n");
+                    // Queue 인터페이스는 getLast()를 사용할 수 없으므로 LinkedList로 형변환하거나 최초 선언을 LinkedList로 해야함.
                     break;
             }
         }
