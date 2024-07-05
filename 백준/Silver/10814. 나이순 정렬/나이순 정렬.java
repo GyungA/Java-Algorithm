563c5c7bcc4aa70bdf1af6dbb8be77bbbd5e89c3
고경아 <gga612@naver.com>
2024-07-06 04:18:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 592 ms, Memory: 47500 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+/*
+ * 회원 객체를 만든다. 필드는 인덱스, 나이, 이름.
+ * 나이순, 나이가 같으면 가입한 순서로 정렬한다.
+ * 나이와 이름을 출력한다.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        ArrayList users = new ArrayList();
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            User user = new User(i, Integer.parseInt(st.nextToken()), st.nextToken());
+            users.add(user);
+        }
+
+        Collections.sort(users);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            User user = (User) users.get(i);
+            sb.append(user.getAge()).append(" ").append(user.getName()).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    public static class User implements Comparable<User>{
+        int index;
+        int age;
+        String name;
+
+        int getAge() {
+            return age;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        User(int index, int age, String name) {
+            this.index = index;
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public int compareTo(User user) {
+            if (user.age == age) {
+                if (user.index < index) {
+                    return 1;
+                } else if (user.index > index) {
+                    return -1;
+                }
+            }
+            if (user.age < age) {
+                return 1;
+            } else if (user.age > age) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
