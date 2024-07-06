7fd522ae01bef28151b672416544519c5947ca31
고경아 <gga612@naver.com>
2024-07-07 01:01:42 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,44 @@
 import java.io.*;
+import java.util.Stack;
 
 /*
- * int sum = 0;
- * 왼괄호를 만나면 sum++;
- * 오른괄호를 만나면 sum--;
- * 괄호가 끝나기 전에 sum < 0 이면 NO,
- * 괄호가 끝나고 나서 sum == 0 ? YES : NO
+ * 괜히 스택으로 한 번 더 풀기
+ * (가 들어오면 '('를 넣는다.
+ * )가 들어오면 '('를 뺀다.
+ * 스택이 비어있는데 빼려고 하면 "NO"
+ * 문자열이 끝났는데 스택이 비워지지 않았으면 "NO"
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+
         int count = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < count; i++) {
-            int sum = 0;
-            String[] ps = br.readLine().split("");
-            for (String p : ps) {
-                sum = (p.equals("(")) ? ++sum : --sum;
-                if (sum < 0) {
-                    break;
+            String parenthesisString = br.readLine();
+            Stack stack = new Stack();
+
+            for (int j = 0; j < parenthesisString.length(); j++) {
+                char currentChar = parenthesisString.charAt(j);
+                if (currentChar == '(') {
+                    stack.push('(');
+                } else if (currentChar == ')') {
+                    if (stack.empty()) {
+                        result.append("NO\n");
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+
+                if (j == parenthesisString.length() -1) {
+                    result.append(stack.empty() ? "YES\n" : "NO\n");
                 }
             }
-            sb.append((sum == 0) ? "YES" : "NO").append("\n");
+
         }
 
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
